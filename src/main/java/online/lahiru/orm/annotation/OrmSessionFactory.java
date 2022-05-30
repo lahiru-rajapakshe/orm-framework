14cc7943fb19465b375c1b66822f476eb7311535
LahiruRJ <sanlahiru720@gmail.com>
2022-05-30 23:42:47 +0530

    Implement entityClassList and method addAnnotationClass

--- src/main/java/online/lahiru/orm/annotation/OrmSessionFactory.java
+++ src/main/java/online/lahiru/orm/annotation/OrmSessionFactory.java
@@ -1,4 +1,21 @@
 package online.lahiru.orm.annotation;
 
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+
 public class OrmSessionFactory {
+
+    private final List<Class<?>> entityClassList = new ArrayList<>();
+    private Connection connection;
+
+    public OrmSessionFactory addAnnotatedClass(Class<?> entityClass){
+        if (entityClass.getDeclaredAnnotation(Entity.class) == null){
+            throw new RuntimeException("Invalid entity class");
+        }
+        entityClassList.add(entityClass);
+        return this;
+    }
+
+
 }
